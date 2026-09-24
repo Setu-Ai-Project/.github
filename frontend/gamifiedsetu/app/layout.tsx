439836f4f3f20ce537561234e4d8a84140772530
Beth Mfune <elizabeth20mfune@gmail.com>
2026-09-24 11:31:38 +0200

    Task 5: BugTooltip mascot component complete

--- frontend/gamifiedsetu/app/layout.tsx
+++ frontend/gamifiedsetu/app/layout.tsx
@@ -3,8 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import {
   ClerkProvider,
-  SignedIn,
-  SignedOut,
+  Show,
   SignInButton,
   SignUpButton,
   UserButton,
@@ -61,15 +60,15 @@ export default function RootLayout({
       >
         <body className="min-h-full flex flex-col">
           <header className="flex justify-end gap-4 p-4">
-            <SignedOut>
+            <Show when="signed-out">
               <SignInButton mode="modal" />
               <SignUpButton mode="modal" />
-            </SignedOut>
+            </Show>
 
-            <SignedIn>
+            <Show when="signed-in">
               <span>Welcome back!</span>
               <UserButton />
-            </SignedIn>
+            </Show>
           </header>
 
           {children}
